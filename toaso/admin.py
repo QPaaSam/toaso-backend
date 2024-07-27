7ea365ed939a0b9e484f916e089f79483a29ef4d
PaaSam <qaugustine08@gmail.com>
2024-07-27 16:14:12 +0000

    created the login,signup and logout functions, started creating the models

--- toaso/admin.py
+++ toaso/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student,Course,CoreSubject,ElectiveSubject, Programs
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(CoreSubject)
+admin.site.register(ElectiveSubject)
+admin.site.register(Programs)
--- toaso/models.py
+++ toaso/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+class CoreSubject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Programs(models.Model):
+    PROGRAM_CHOICES = [
+        ('SCIENCE', 'GENERAL SCIENCE'),
+        ('BUSINESS', 'BUSINESS'),
+        ('G.ARTS', 'GENERAL ARTS'),
+        ('V.ARTS', 'VISUAL ARTS'),
+        ('HOME_ECONS', 'HOME ECONOMICS')
+    ]
+    name = models.CharField(max_length=50, choices=PROGRAM_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.get_name_display()
+
+class ElectiveSubject(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Programs, on_delete=models.CASCADE, related_name='elective_subjects', null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    high_school_courses = models.TextField()
+    interests = models.TextField()
+    core_subjects = models.ManyToManyField(CoreSubject)
+    program = models.ForeignKey(Programs, on_delete=models.SET_NULL, null=True)
+    elective_subjects = models.ManyToManyField(ElectiveSubject)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    cutt_off_point = models.SmallIntegerField()
+    career = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
