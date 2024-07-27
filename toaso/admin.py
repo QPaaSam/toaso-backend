6f1f2c08b629de3e749ae78a9782eaa3b3428af4
PaaSam <92068176+QPaaSam@users.noreply.github.com>
2024-07-27 17:03:06 +0000

    edited the models models editted

--- toaso/admin.py
+++ toaso/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Student,Course,CoreSubject,ElectiveSubject, Programs
+from .models import Student,Course
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Course)
-admin.site.register(CoreSubject)
-admin.site.register(ElectiveSubject)
-admin.site.register(Programs)
--- toaso/models.py
+++ toaso/models.py
@@ -1,41 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class CoreSubject(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Programs(models.Model):
-    PROGRAM_CHOICES = [
-        ('SCIENCE', 'GENERAL SCIENCE'),
-        ('BUSINESS', 'BUSINESS'),
-        ('G.ARTS', 'GENERAL ARTS'),
-        ('V.ARTS', 'VISUAL ARTS'),
-        ('HOME_ECONS', 'HOME ECONOMICS')
-    ]
-    name = models.CharField(max_length=50, choices=PROGRAM_CHOICES, unique=True)
-
-    def __str__(self):
-        return self.get_name_display()
-
-class ElectiveSubject(models.Model):
-    name = models.CharField(max_length=100)
-    group = models.ForeignKey(Programs, on_delete=models.CASCADE, related_name='elective_subjects', null=True)
-
-    def __str__(self):
-        return self.name
-    
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    high_school_courses = models.TextField()
     interests = models.TextField()
-    core_subjects = models.ManyToManyField(CoreSubject)
-    program = models.ForeignKey(Programs, on_delete=models.SET_NULL, null=True)
-    elective_subjects = models.ManyToManyField(ElectiveSubject)
+    elective_subjects = models.TextField(null=True)
 
     def __str__(self):
         return self.name
@@ -47,6 +18,7 @@ class Course(models.Model):
     requirements = models.TextField()
     cutt_off_point = models.SmallIntegerField()
     career = models.CharField(max_length=100)
+    note = models.TextField(null=True)
 
     def __str__(self):
         return self.title
